b98828331e296dc7a993576183524e005ca0b660
X_Mingest <x_mingest@163.com>
2017-03-07 20:48:25 +0800

    SQL big edit

--- AoNews/mysql/aonewsdb_init.sql
+++ AoNews/mysql/aonewsdb_init.sql
@@ -1,6 +1,6 @@
 # Environment: mysql
 # create schema
-create database aonews;
+create database aonews default charset = utf8;
 use aonews;
 
 # Login Info
@@ -8,19 +8,19 @@ use aonews;
 # Password less than 20 characters
 create table Login(
 	UserName char(20),
-	Password char(20) not null,
-	Mail varchar(50) not null,
+	Password char(20),
+	Mail varchar(50),
 	primary key(UserName)
-)default charset = utf8;
+)
 
 # Keyword Info
 # Word less than 10 characters
 # HotIndex >= && HotIndex <= 255
 create table Keyword(
 	Word char(10),
-	HotIndex tinyint unsigned not null,
+	HotIndex int unsigned,
 	primary key(Word)
-)default charset = utf8;
+)
 
 # News Info
 # Local url: news/ + UrlID + .html
@@ -29,11 +29,11 @@ create table Keyword(
 # Publish time( yyyy-mm-dd hh:mm:ss )
 create table News(
 	UrlID char(10),
-	Title varchar(30) not null,
-	SrcWebsite varchar(50) not null,
-	PublishTime varchar(30) not null,
+	Title varchar(30),
+	SrcWebsite varchar(50),
+	PublishTime varchar(30),
 	primary key(UrlID)
-)default charset = utf8;
+)
 
 # User read keyword history
 create table UserKeyword(
@@ -42,7 +42,7 @@ create table UserKeyword(
 	primary key(UserName,Word),
 	foreign key(UserName) references Login(UserName),
 	foreign key(Word) references Keyword(Word)
-)default charset = utf8;
+)
 
 # News write keyword record
 create table NewsKeyword(
@@ -51,4 +51,14 @@ create table NewsKeyword(
 	primary key(UrlID,Word),
 	foreign key(UrlID) references News(UrlID),
 	foreign key(Word) references Keyword(Word)
-)default charset = utf8;
+)
+
+# User read News record
+create table UserNews(
+	UserName char(20),
+	UrlID char(10),
+	CallTime timestamp default current_timestamp,
+	primary key(UserName, UrlID),
+	foreign key(UserName) references Login(UserName),
+	foreign key(UrlID) references News(UrlID)
+)
