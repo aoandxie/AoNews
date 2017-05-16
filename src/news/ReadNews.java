1ca6b0d1a4d718f186960a923f42413fa9bf1a49
X_Mingest <x_mingest@163.com>
2017-05-16 21:12:49 +0800

    resp.Utf-8 ensure

--- src/news/ReadNews.java
+++ src/news/ReadNews.java
@@ -19,30 +19,42 @@ public class ReadNews extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		/*******************************/
-		/*标题我是不知道的，所以随便打了一个*/
+		/* 标题我是不知道的，所以随便打了一个 */
 		/*******************************/
 		NewsHtml html = new NewsHtml("need to select from database");
-		
+
 		/******************************************************/
-		/* 读取localhost:8080/AoNews/ReadNews?newsid=后面的那部分
-		 * 后期应该修改为相应的数据库主键
-		 * 暂时先直接输入新闻目录下的文件夹 */
+		/*
+		 * 读取localhost:8080/AoNews/ReadNews?newsid=后面的那部分 后期应该修改为相应的数据库主键
+		 * 暂时先直接输入新闻目录下的文件夹
+		 */
 		/******************************************************/
 		String newsid = req.getParameter("newsid");
-		
+
 		/**********************************************/
-		/*图片以及文字路径，暂时没有合成一个，可以在这里改一下*/
+		/* 图片以及文字路径，暂时没有合成一个，可以在这里改一下 */
 		/**********************************************/
-		File imgPath = new File("/home/user/apache/apache-tomcat-6.0.53/webapps/AoNews/MyCrawl/" + newsid + "/Img/");
-		File textPath = new File("/home/user/apache/apache-tomcat-6.0.53/webapps/AoNews/MyCrawl/" + newsid + "/Text/");
-		
-		if (!imgPath.isDirectory() || !textPath.isDirectory()) {
-			JOptionPane.showMessageDialog(null, "文件夹不存在");
+		File imgPath = new File(
+				"/home/xmingest/apache/apache-tomcat-6.0.53/webapps/AoNews/MyCrawl/" + newsid + "/Img/");
+		File textPath = new File(
+				"/home/xmingest/apache/apache-tomcat-6.0.53/webapps/AoNews/MyCrawl/" + newsid + "/Text/");
+
+		// 设置返回的编码集
+		resp.setContentType("text/html; charset=utf8");
+
+		if (!imgPath.isDirectory() && !textPath.isDirectory()) {
+			JOptionPane.showMessageDialog(null, "文件夹不存在"); // 确定Img或Text文件夹在指定目录下存在
 		} else {
+			// 读取Img与Text目录下的文件名
 			String[] imgList = imgPath.list();
 			String[] textList = textPath.list();
+
+			// 循环跳跃判定
 			boolean found = false;
+
+			// 循环搜索文件或者图片
 			for (int i = 0; i < imgList.length + textList.length; i++) {
+				// 图片区
 				for (String img : imgList)
 					if (img.matches(i + ".*")) {
 						html.addImgBody("MyCrawl/" + newsid + "/Img/" + img);
@@ -53,8 +65,10 @@ public class ReadNews extends HttpServlet {
 					found = false;
 					continue;
 				}
+
+				// 文件区
 				for (String text : textList)
-					if (text.matches(i + ".*")) {
+					if (text.matches(i + "[.].*")) {
 						File textFile = new File(textPath + "/" + text);
 						if (!textFile.isFile()) {
 							JOptionPane.showMessageDialog(null, "文件不存在");
